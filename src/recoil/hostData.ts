4583cbdbfd089eac7c1951255aa561b7cbd2d02a
hae-on <solbi2004@naver.com>
2023-06-12 19:16:11 +0900

    chore: 기본 서버 수정

--- src/recoil/hostData.ts
+++ src/recoil/hostData.ts
@@ -5,7 +5,7 @@ import { orderApi } from '../apis/orderProducts';
 
 export const hostNameAtom = atom<HostNameType>({
   key: 'hostNameState',
-  default: '도기',
+  default: '해온',
 });
 
 export const cartApiAtom = selector({
